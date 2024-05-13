1db8907165795a554f45a225604fafeb5190cb96
Leonardo Fazekas <leonardofazekas@gmail.com>
2024-05-13 15:51:03 -0300

    create auth with login

--- app_oracoes/lib/main.dart
+++ app_oracoes/lib/main.dart
@@ -1,7 +1,24 @@
 import 'package:app_oracoes/screens/inicial_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:nhost_dart/nhost_dart.dart';
+import 'package:nhost_flutter_auth/nhost_flutter_auth.dart';
+import 'package:nhost_flutter_graphql/nhost_flutter_graphql.dart';
 
-void main() {
+void main() async {
+  final nhost = NhostClient(
+      serviceUrls: ServiceUrls(
+          authUrl: 'https://mndfdrqkrknemcyfsfge.auth.sa-east-1.nhost.run/v1',
+          graphqlUrl:
+              'https://mndfdrqkrknemcyfsfge.graphql.sa-east-1.nhost.run/v1',
+          functionsUrl:
+              'https://mndfdrqkrknemcyfsfge.functions.sa-east-1.nhost.run/v1',
+          storageUrl:
+              'https://mndfdrqkrknemcyfsfge.storage.sa-east-1.nhost.run/v1')
+      // subdomain: Subdomain(
+      //   region: 'sa-east-1',
+      //   subdomain: 'mndfdrqkrknemcyfsfge',
+      );
+
   runApp(const MaterialApp(
     home: InicalScreen(),
   ));
--- app_oracoes/lib/screens/login_screen.dart
+++ app_oracoes/lib/screens/login_screen.dart
@@ -1,72 +1,110 @@
 import 'package:app_oracoes/screens/lista_oracoes.dart';
 import 'package:flutter/material.dart';
+import 'package:nhost_dart/nhost_dart.dart';
 
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          color: Colors.black,
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const TextField(
-                decoration: InputDecoration(
-                  labelText: 'E-mail',
-                  labelStyle: TextStyle(color: Colors.white),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
-                  ),
-                ),
-                style: TextStyle(color: Colors.white),
+    return const MaterialApp(
+      home: LoginScreen(),
+    );
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(
+                labelText: 'Email',
               ),
-              const SizedBox(height: 16.0),
-              const TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Senha',
-                  labelStyle: TextStyle(color: Colors.white),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
-                  ),
-                ),
-                style: TextStyle(color: Colors.white),
+            ),
+            const SizedBox(height: 12),
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(
+                labelText: 'Password',
               ),
-              const SizedBox(height: 32.0),
-              ElevatedButton(
-                onPressed: () {
-                  context;
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const ListOracoes()));
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.red,
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 100,
-                    vertical: 20,
-                  ),
-                ),
-                child: const Text(
-                  'Login',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-            ],
-          ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _login,
+              child: const Text('Login'),
+            ),
+          ],
         ),
       ),
     );
+  }
+
+  void _login() async {
+    final client = NhostClient(
+      subdomain: Subdomain(
+        subdomain: 'mndfdrqkrknemcyfsfge',
+        region: 'sa-east-1',
+      ),
+    );
+    try {
+      await signInOrSignUp(
+        client,
+        email: _emailController.text.trim(),
+        password: _passwordController.text.trim(),
+      );
+      // Redirecionar para a tela ListOracoes após o login bem-sucedido
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const ListOracoes()),
+      );
+    } catch (e) {
+      print(e);
+    }
+    // Release
+    client.close();
+  }
+
+  Future<void> signInOrSignUp(
+    NhostClient client, {
+    required String email,
+    required String password,
+  }) async {
+    try {
+      await client.auth.signInEmailPassword(email: email, password: password);
+      return;
+    } on ApiException catch (e) {
+      print('Sign in failed, so try to register instead');
+      print(e);
+      await client.auth.signUp(email: email, password: password);
+    } catch (e, st) {
+      print(e);
+      print(st);
+    }
   }
 }
